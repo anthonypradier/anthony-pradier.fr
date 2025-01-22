d84956fc42b915de957c0ac49a5a56a07aac5391
Anthony <anthony.pradier04@gmail.com>
2025-01-22 17:43:34 +0100

    test for an env file

--- resources/js/index.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById('contact-form');
-const ph = document.querySelectorAll('.form-item');
-const phBefore = window.getComputedStyle(form, '::before');
-
-console.log(phBefore.color);
-for(let elt of ph) {
-    console.log('elt');
-    
-}
